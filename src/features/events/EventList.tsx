e6fbdadf7b3d9ed7827e4f327dc22441bfd5dc51
Jefferson Uchoa Ponte <jefponte@gmail.com>
2023-08-29 17:07:34 -0300

    Add slice

--- src/features/events/EventList.tsx
+++ src/features/events/EventList.tsx
@@ -1,22 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../../app/hooks";
 import { selectEvents } from "./eventSlice";
-// import { fetchData } from "../../services/api";
 
 export function EventList() {
+  const columns = [
+    { field: 'id', headerName: "Nº", width: 100 },
+    { field: 'area', headerName: "Área", width: 200 },
+    { field: 'objetivo', headerName: "Objetivo", width: 200 },
+    { field: 'classificacaoIndicador', headerName: "Classificação do Indicador", width: 200 },
+    { field: 'categoriaIndicador', headerName: "Categoria do Indicador" },
+    { field: 'tipoIndicador', headerName: "Tipo de Indicador" },
+    { field: 'descricaoIndicador', headerName: "Descrição do Indicador", width: 250 },
+    { field: 'descricao', headerName: "Descrição da Meta", width: 250 },
+    { field: 'prazo', headerName: "Prazo", width: 100 },
+    { field: 'unidadeResponsavel', headerName: "Unidade Responsavel", width: 200 },
+    { field: 'unidadeCoResponsavel', headerName: "Unidade Co-Responsavel", width: 250 }
+  ];
 
-  // const [events, setevEnts] = useState([]);
-  // useEffect(() => {
-  //   fetchData(setevEnts);
-  // }, []);
-  // console.log(events);
   const events = useAppSelector(selectEvents);
 
   return (
     <div>
       Event List
       {events.map((element) => {
-        return (<p>{element.area}</p>);
+        return (<p key={element.id}>{element.area}</p>);
       })}
     </div>
   )
--- src/features/events/eventSlice.ts
+++ src/features/events/eventSlice.ts
@@ -1,5 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
+import axios from "axios";
+
+
+export const api = axios.create({
+    baseURL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTDX4Iiart_C_hlcFQeBl-GyGMInu9s7wr24yETCoJkStH5ZPlUOrfQw2yTw6Zv9vBNCbw2jKIElCDv/pub?gid=0&single=true&output=tsv'
+});
+
+function tsvToJSON(tsv: string) {
+    const lines = tsv.split('\r\n');
+    const result: Record<string, string>[] = [];
+    const headers = lines[0].split('\t');
+    for (let i = 1; i < lines.length; i++) {
+        if (!lines[i]) {
+            continue;
+        }
+        const obj: Record<string, string> = {};
+        const currentline = lines[i].split('\t');
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    return result;
+}
+
 
 interface Event {
     id: string,
@@ -7,17 +32,11 @@ interface Event {
     process: string;
 }
 
-const event = {
-    "id": "3",
-    "area": "Ouvidoria",
-    "process": "Manifestações de Ouvidoria"
-}
 
+const response = await api.get('');
+const events = tsvToJSON(response.data);
 
-export const initialState = [event,
-    { ...event, id: "4" },
-    { ...event, id: "5" },
-    { ...event, id: "6" }];
+export const initialState = events;
 
 const eventSlice = createSlice({
     name: "events",
